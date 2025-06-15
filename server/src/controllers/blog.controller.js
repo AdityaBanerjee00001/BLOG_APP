0cec99ae5df48f2e7cd9a1954a5d9c1c251d0a70
AdityaBanerjee00001 <banerjeeaditya445@example.com>
2025-06-15 20:56:22 +0530

    blog controller update blogs

--- server/src/controllers/blog.controller.js
+++ server/src/controllers/blog.controller.js
@@ -18,7 +18,6 @@ export const getAllBlogs = asyncHandler(async (req, res, next) => {
   return res.status(200).json(new ApiResponse(200, blogs, `All Blogs Fetched`));
 });
 
-
 export const getFeedBlogs = asyncHandler(async (req, res, next) => {});
 
 export const getBlogById = asyncHandler(async (req, res, next) => {
@@ -48,11 +47,11 @@ export const createBlogs = asyncHandler(async (req, res, next) => {
   if (!title || !description) {
     throw new ApiError(400, "All fields are required");
   }
-  const existingBlog = await Blog.findOne({ title,author: userId });
+  const existingBlog = await Blog.findOne({ title, author: userId });
   if (existingBlog) {
     throw new ApiError(400, "Blog with same title exists");
   }
-  const newBlog = await Blog.create({ title, description, author: userId});
+  const newBlog = await Blog.create({ title, description, author: userId });
   user.blogs.push(newBlog._id);
   await user.save();
   return res
@@ -66,21 +65,66 @@ export const createBlogs = asyncHandler(async (req, res, next) => {
     );
 });
 
-export const updateBlogs = asyncHandler(async (req, res, next) => {});
-
-export const deleteBlogs = asyncHandler(async (req, res, next) => {
+export const updateBlogs = asyncHandler(async (req, res, next) => {
+  const { title, description, content, tags, isPublished } = req.body;
   const blogId = req.params.id;
   const userId = req.id;
-  const blog = await Blog.findOne({_id:blogId,author:userId});
+  const blog = await Blog.findById(blogId);
   if (!blog) {
     throw new ApiError(404, "Blog not found");
   }
-  await Blog.deleteOne({_id:blogId,author:userId});
-
-  await User.findByIdAndUpdate(userId,{$pull:{blogs:blogId}})
+  if (blog.author.toString() !== userId) {
+    throw new ApiError(403, "You are not authorized to update this blog");
+  }
+  let coverImageUrl;
+  if (req.files && req.files.coverImage) {
+    console.log(req.files);
+    coverImageUrl = req.files.coverImage[0].path;
+  }
+  if (coverImageUrl) {
+    if (blog.coverImage) {
+      await deleteOnCloudinary(blog.coverImage.public_id);
+    }
+    const result = await uploadOnCloudinary(coverImageUrl, "blog-covers");
+    blog.coverImage.url = result.secure_url;
+    blog.coverImage.public_id = result.public_id;
+  }
+  if (title) blog.title = title;
+  if (description) blog.description = description;
+  if (content) blog.content = content;
+  if (tags)
+    blog.tags = Array.isArray(tags)
+      ? tags
+      : tags.split(",").map((tag) => tag.trim());
+  if (isPublished !== undefined) {
+    blog.isPublished = isPublished;
+    if (isPublished && !blog.publishedAt) {
+      blog.publishedAt = new Date();
+    }
+  }
+  if (content) {
+    const wordsPerMinute = 50;
+    const wordCount = content.trim().split(/\s+/).length;
+    blog.readTime = Math.ceil(wordCount / wordsPerMinute);
+  }
+  await blog.save();
   return res
-  .status(200)
-  .json(new ApiResponse(200,{},"Blog Deleted Successfully"));
+    .status(200)
+    .json(new ApiResponse(200, blog, "Blog updated successfully"));
+});
+export const deleteBlogs = asyncHandler(async (req, res, next) => {
+  const blogId = req.params.id;
+  const userId = req.id;
+  const blog = await Blog.findOne({ _id: blogId, author: userId });
+  if (!blog) {
+    throw new ApiError(404, "Blog not found");
+  }
+  await Blog.deleteOne({ _id: blogId, author: userId });
+
+  await User.findByIdAndUpdate(userId, { $pull: { blogs: blogId } });
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Blog Deleted Successfully"));
 });
 
 export const toggleLike = asyncHandler(async (req, res, next) => {
@@ -98,7 +142,6 @@ export const toggleLike = asyncHandler(async (req, res, next) => {
     user: userId,
     target: "blog",
     targetId: blogId,
-    
   });
   let message;
   if (existingLike) {
@@ -127,9 +170,3 @@ export const toggleLike = asyncHandler(async (req, res, next) => {
   }
   return res.status(200).json(new ApiResponse(200, null, message));
 });
-
-export const addComment = asyncHandler(async (req, res, next) => {
-  
-});
-
-export const getComment = asyncHandler(async (req, res, next) => {});
--- server/src/routes/blog.routes.js
+++ server/src/routes/blog.routes.js
@@ -1,17 +1,22 @@
 import { Router } from "express";
-import { addComment, createBlogs, deleteBlogs, getAllBlogs, getBlogById, getComment, getFeedBlogs, toggleLike, updateBlogs } from "../controllers/blog.controller.js";
+import {
+  createBlogs,
+  deleteBlogs,
+  getAllBlogs,
+  getBlogById,
+  getFeedBlogs,
+  toggleLike,
+  updateBlogs,
+} from "../controllers/blog.controller.js";
 import { isAuthenticated } from "../middlewares/auth.middleware.js";
 
 const blogRouter = Router();
 
-blogRouter.get("/allblogs",isAuthenticated,getAllBlogs);
-blogRouter.get("/feed",getFeedBlogs);
-blogRouter.get("/:id",isAuthenticated,getBlogById);
-blogRouter.post("/create",isAuthenticated,createBlogs);
-blogRouter.put("/update", updateBlogs);
-blogRouter.delete("/delete/:id",isAuthenticated,deleteBlogs);
-blogRouter.post("/like/:id",isAuthenticated, toggleLike);
-blogRouter.post("/comment/:id", addComment);
-blogRouter.get("/comments",getComment);
-
-export default blogRouter;
+blogRouter.get("/allblogs", isAuthenticated, getAllBlogs);
+blogRouter.get("/feed", getFeedBlogs);
+blogRouter.get("/:id", isAuthenticated, getBlogById);
+blogRouter.post("/create", isAuthenticated, createBlogs);
+blogRouter.put("/update/:id",isAuthenticated,updateBlogs);
+blogRouter.delete("/delete/:id", isAuthenticated, deleteBlogs);
+blogRouter.post("/like/:id", isAuthenticated, toggleLike);
+export default blogRouter;
